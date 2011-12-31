00a16ff946d531683b6e10b1484d57a8a78eea9a
Kyle Stewart <kylestew@gmail.com>
2011-12-30 18:12:10 -0700

    Basic layout established

--- Soundboard/src/com/grilledcheeseapps/soundboard/SoundboardActivity.java
+++ Soundboard/src/com/grilledcheeseapps/soundboard/SoundboardActivity.java
@@ -1,13 +1,15 @@
-package com.grilledcheeseapps;
+package com.grilledcheeseapps.soundboard;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.grilledcheeseapps.soundboard.R;
 
 public class SoundboardActivity extends Activity {
-    /** Called when the activity is first created. */
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.view_soundboard);
     }
+    
 }
