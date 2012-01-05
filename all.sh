f95d5556b0c9fb035f7f85508311cd47630419a4
Kyle Stewart <kylestew@gmail.com>
2012-01-04 19:31:45 -0700

    Pushed through all todos on FMF

--- all.sh
+++ all.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 #Version code
-VC=1
+VC=2
 #Version Name
-VN=1.0
+VN=1.2
 
 echo "\n"
 echo "Building All Soundboards for version $VN \n"
 
 ./build.sh "Fantastic Mr. Fox Soundboard" fantastic_mr_fox $VC $VN
 
-#./build.sh "Scott Pilgrim Soundboard" scott_pilgrim $VC $VN
+./build.sh "Scott Pilgrim Soundboard" scott_pilgrim $VC $VN
 
-#./build.sh "Napoleon Dynamite Soundboard" napoleon_dynamite $VC $VN
+./build.sh "Napoleon Dynamite Soundboard" napoleon_dynamite $VC $VN
 
-#./build.sh "Nacho Libre Soundboard" nacho_libre $VC $VN
+./build.sh "Nacho Libre Soundboard" nacho_libre $VC $VN
