7fe4906136717f2c1d3f93059d0e2e0858057c1b
Kyle Stewart <kylestew@gmail.com>
2012-01-05 11:05:41 -0700

    Ready to push to the Market

--- all.sh
+++ all.sh
@@ -8,10 +8,10 @@ VN=1.2
 echo "\n"
 echo "Building All Soundboards for version $VN \n"
 
-./build.sh "Fantastic Mr. Fox Soundboard" fantastic_mr_fox $VC $VN
+#./build.sh "Fantastic Mr. Fox Soundboard" fantastic_mr_fox $VC $VN
 
 #./build.sh "Scott Pilgrim Soundboard" scott_pilgrim $VC $VN
 
-#./build.sh "Napoleon Dynamite Soundboard" napoleon_dynamite $VC $VN
+./build.sh "Napoleon Dynamite Soundboard" napoleon_dynamite $VC $VN
 
 #./build.sh "Nacho Libre Soundboard" nacho_libre $VC $VN
